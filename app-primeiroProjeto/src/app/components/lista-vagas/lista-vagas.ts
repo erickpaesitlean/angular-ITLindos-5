40857ebba25b8278a2e610b3ca993d06013aa34f
erickpaesitlean <erick.paes@itlean.com.br>
2025-11-06 10:54:40 -0300

    Update lista-vagas.ts

--- app-primeiroProjeto/src/app/components/lista-vagas/lista-vagas.ts
+++ app-primeiroProjeto/src/app/components/lista-vagas/lista-vagas.ts
@@ -1,4 +1,4 @@
-import { Component, WritableSignal , signal } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
 
 @Component({
   selector: 'app-lista-vagas',
@@ -7,6 +7,7 @@ import { Component, WritableSignal , signal } from '@angular/core';
   styleUrl: './lista-vagas.scss',
 })
 export class ListaVagas {
+
   contador: WritableSignal<number> = signal(0)
   add():void{
     this.contador.update(valorAntigo => valorAntigo + 1)
@@ -15,4 +16,5 @@ export class ListaVagas {
     'Erick',
     'Theo'
   ]
+
 }
